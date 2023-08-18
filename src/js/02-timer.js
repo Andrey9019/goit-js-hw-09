f4d8ed8e9c81819fe7e0fb15d10b4b7d14718e0d
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-08-19 01:37:51 +0300

    add task -01

---
+++ src/js/02-timer.js
@@ -0,0 +1,26 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const datePicker = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const daysValue = document.querySelector('[data-days]');
+const hoursValue = document.querySelector('[data-hours]');
+const minutesValue = document.querySelector('[data-minutes]');
+const secondsValue = document.querySelector('[data-seconds]');
+
+const currentDate = new Date();
+
+startBtn.disable = true;
+let timerID;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(datePicker, options);
